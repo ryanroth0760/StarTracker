7b05ff2f2f63ea3ea56cf1a09838a43ea65271be
Ryan Roth <ryan.roth15@hotmail.com>
2022-03-01 11:50:13 -0800

    blinking LED test

--- main.c
+++ main.c
@@ -4,5 +4,15 @@ int main(void) {
 
     WDT_A_hold(WDT_A_BASE);
 
+    //use Timer_A to blink LED, simplest test program
+    //initialize IO output
+    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+
+    while(1) {
+        __delay_us(1000);
+        GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    }
+
     return (0);
 }
