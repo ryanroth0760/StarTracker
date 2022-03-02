47cb2b1ab09b2897bbae59b35f01d009bb9f7cf2
ryanroth0760 <ryanroth@ufl.edu>
2022-03-02 11:08:21 -0500

    comments

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "driverlib.h"
 
 int main(void) {
+    WDT_A_hold(WDT_A_BASE); //disable watchdog timer
 
-    WDT_A_hold(WDT_A_BASE);
+    //use delay loop to blink LED as first test program
+
+    //then use rotary encoder to control PWM pin as second test program. Test on LED
 
     return (0);
 }
